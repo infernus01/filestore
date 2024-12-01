e002e2fd747a543d80719c352b28d88ced41ecfc
Shubham Bhardwaj <sbmbhardwaj01@gmail.com>
2024-12-01 16:27:09 +0530

    Implement Cobra for Command Line Client

---
+++ cmd/file-server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	filehandler "github.com/infernus01/fileService/pkg/fileHandler"
+)
+
+func main() {
+	http.HandleFunc("/list", filehandler.HandleListFile)
+	http.HandleFunc("/add", filehandler.HandleAddFile)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
